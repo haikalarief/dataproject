83a5f5cf3123f8975080256fce8c466b05cd6bdd
haikalarief <haikal.arief@gmail.com>
2025-06-09 14:22:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="World Happiness Score", layout="wide")
 #load cleaned data
 df = pd.read_csv("cleaned_happiness.csv")
 
-st.title("🌍 World HappinessDashboard")
+st.title("🌍 World Happiness Dashboard")
 
 #sidebar utk filter data
 st.sidebar.header("Filter Options")
